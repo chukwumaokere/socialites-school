d110a6a1b32ff400f55f02ea082caaa4538d90c3
chukwumaokere <chukwumaokere@yahoo.com>
2020-10-11 17:56:56 -0500

    add 2 new pages

---
+++ src/views/Plans.vue
@@ -0,0 +1,23 @@
+<template>
+  <Navbar />
+  <Container>
+    <div>
+        Plans
+    </div>
+  </Container>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar"
+import Container from "@/components/Container";
+export default {
+    components:{ 
+        Navbar,
+        Container,
+    }
+}
+</script>
+
+<style>
+
+</style>
